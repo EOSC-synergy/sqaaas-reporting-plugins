366acb80d72c57c813b81f9252b1fde24c55ebb7
Pablo Orviz <orviz@ifca.unican.es>
2021-11-16 14:01:20 +0100

    Check if licensee output is valid

--- qc_lic_licensee/qc_lic_licensee/main.py
+++ qc_lic_licensee/qc_lic_licensee/main.py
@@ -2,9 +2,24 @@ from report2sqaaas import utils as sqaaas_utils
 
 
 class LicenseeValidator(sqaaas_utils.BaseValidator):
+    valid = False
+
     def parse(self, file_name):
         return sqaaas_utils.load_json(file_name)
 
     def validate(self, file_name):
         print('Running LicenseeValidator..')
         data = self.parse(file_name)
+        at_least_one_license = False
+        trusted_licenses_no = 0
+        for license_data in data['matched_files']:
+            if license_data['matcher']['confidence'] > 50:
+                at_least_one_license = True
+                trusted_licenses_no += 1
+        if at_least_one_license:
+            self.valid = True
+
+        return {
+            'valid': self.valid,
+            'data_unstructured': data
+        }
