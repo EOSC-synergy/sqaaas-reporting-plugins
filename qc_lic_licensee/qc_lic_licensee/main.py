2185642a45d2056fd6c901b112eb02cc436c4269
Pablo Orviz <orviz@ifca.unican.es>
2021-11-16 13:59:35 +0100

    Parse the JSON as the input arg

--- qc_lic_licensee/qc_lic_licensee/main.py
+++ qc_lic_licensee/qc_lic_licensee/main.py
@@ -2,5 +2,9 @@ from report2sqaaas import utils as sqaaas_utils
 
 
 class LicenseeValidator(sqaaas_utils.BaseValidator):
+    def parse(self, file_name):
+        return sqaaas_utils.load_json(file_name)
+
     def validate(self, file_name):
         print('Running LicenseeValidator..')
+        data = self.parse(file_name)
