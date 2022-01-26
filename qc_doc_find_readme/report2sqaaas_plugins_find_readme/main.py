765eaf5ecd8e13dec4a21609dde2928553cb64b0
Pablo Orviz <orviz@ifca.unican.es>
2022-01-26 12:36:44 +0100

    Fix: validation and provide reason

--- qc_doc_find_readme/report2sqaaas_plugins_find_readme/main.py
+++ qc_doc_find_readme/report2sqaaas_plugins_find_readme/main.py
@@ -11,26 +11,42 @@ class FindReadmeValidator(sqaaas_utils.BaseValidator):
     threshold = 1
 
     def validate(self):
+        reason = None
         try:
             readme_data_list = sqaaas_utils.load_json(self.opts.stdout)
+            logger.debug('Parsing output: %s' % readme_data_list)
         except ValueError:
             readme_data_list = []
-            logger.error('Input data does not contain a valid JSON')
-
-        self.valid = True
-        if readme_data_list:
-            for readme_data in readme_data_list:
-                for readme_file, readme_size in readme_data.items():
-                    if readme_size['size'] < self.threshold:
-                        logger.warn((
-                            'README file <%s> is not big enough (self.threshold '
-                            '%s)' % (readme_file, self.threshold)
-                        ))
-                        self.valid = False
+            reason = 'Input data does not contain a valid JSON'
+            logger.error(reason)
         else:
-            logger.warn('JSON payload is empty')
+            if readme_data_list:
+                self.valid = True
+                for readme_data in readme_data_list:
+                    for readme_file, readme_size in readme_data.items():
+                        if readme_size['size'] < self.threshold:
+                            logger.warn((
+                                'File <%s> is not big enough (self.threshold '
+                                '%s)' % (readme_file, self.threshold)
+                            ))
+                            self.valid = False
+                        else:
+                            logger.debug((
+                                'Size good enough for <%s> collaboration-enabling '
+                                'file: %s bytes' % (
+                                    readme_file,
+                                    readme_size
+                                )
+                            ))
+            else:
+                reason = 'JSON payload is empty'
+                logger.warn(reason)
 
-        return {
+        out = {
             'valid': self.valid,
             'data_unstructured': readme_data_list
         }
+        if reason:
+            out['reason'] = reason
+
+        return  out
