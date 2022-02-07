20ee512c9906032207ee247a38f7c8b5f27563ef
Pablo Orviz <orviz@ifca.unican.es>
2022-02-07 07:47:58 +0100

    Return report claims

--- qc_lic_licensee/report2sqaaas_plugins_licensee/main.py
+++ qc_lic_licensee/report2sqaaas_plugins_licensee/main.py
@@ -14,27 +14,38 @@ class LicenseeValidator(sqaaas_utils.BaseValidator):
         return sqaaas_utils.load_json(file_name)
 
     def validate(self):
+        report = []
         try:
             data = self.parse(self.opts.stdout)
             at_least_one_license = False
             trusted_licenses_no = 0
             for license_data in data['matched_files']:
+                file_name = license_data['filename']
                 if not license_data.get('matcher', None):
                     logger.warn(
                         'Matcher data not found for file <%s>. '
-                        'Skipping..' % license_data['filename']
+                        'Skipping..' % file_name
                     )
                     continue
-                if license_data['matcher']['confidence'] > self.threshold:
+                confidence_level = license_data['matcher']['confidence']
+                if confidence_level > self.threshold:
                     at_least_one_license = True
                     trusted_licenses_no += 1
             if at_least_one_license:
                 self.valid = True
+                report.append(
+                    'Valid LICENSE found (confidence level: %s): %s' % (
+                        confidence_level, file_name
+                    )
+                )
+            else:
+                report.append('No valid LICENSE found')
         except ValueError as e:
             data = {}
-            logger.error('Input data does not contain a valid JSON: %s' % e)
+            report.append('Input data does not contain a valid JSON: %s' % e)
 
         return {
             'valid': self.valid,
+            'report': report,
             'data_unstructured': data
         }
