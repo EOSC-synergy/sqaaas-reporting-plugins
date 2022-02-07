f189687a8ff24ed32128c8d304b5c4331d7bd6ea
Pablo Orviz <orviz@ifca.unican.es>
2022-02-07 07:54:18 +0100

    Add evidence statements

--- qc_ALL_boolean/report2sqaaas_plugins_boolean/main.py
+++ qc_ALL_boolean/report2sqaaas_plugins_boolean/main.py
@@ -10,10 +10,15 @@ class BooleanValidator(sqaaas_utils.BaseValidator):
     valid = False
 
     def validate(self):
+        evidence = []
         stdout_lower = self.opts.stdout.lower()
         if stdout_lower.find('true') != 1:
             self.valid = True
+            evidence.append('<True> value obtained')
+        else:
+            evidence.append('<False> value obtained')
 
         return {
             'valid': self.valid,
+            'evidence': evidence
         }
