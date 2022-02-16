4812f768e4dd557398e0cbf17f4422f324a89abe
Pablo Orviz <orviz@ifca.unican.es>
2022-02-16 07:51:33 +0100

    Report subcriteria for markdownlint plugin

--- qc_doc_markdownlint/report2sqaaas_plugins_markdownlint/main.py
+++ qc_doc_markdownlint/report2sqaaas_plugins_markdownlint/main.py
@@ -8,8 +8,27 @@ logger = logging.getLogger('sqaaas.reporting.plugins.markdownlint')
 
 class MarkdownLintValidator(sqaaas_utils.BaseValidator):
     valid = False
+    standard = {
+        'title': (
+            'A set of Common Software Quality Assurance Baseline Criteria for '
+            'Research Projects'
+        ),
+        'version': 'v4.0',
+        'url': 'https://github.com/indigo-dc/sqa-baseline/releases/tag/v4.0',
+    }
 
     def validate(self):
+        criterion = 'QC.Sty'
+        criterion_data = sqaaas_utils.load_criterion_from_standard(
+            criterion
+        )
+        subcriteria = []
+        # QC.Sty01
+        subcriterion = 'QC.Sty01'
+        subcriterion_data = criterion_data[subcriterion]
+        subcriterion_valid = False
+        evidence = None
+
         try:
             data = sqaaas_utils.load_json(self.opts.stdout)
         except ValueError:
@@ -17,10 +36,32 @@ class MarkdownLintValidator(sqaaas_utils.BaseValidator):
             logger.error('Input data does not contain a valid JSON')
 
         if not data:
-            logger.info('No issue found by markdownlint!')
-            self.valid = True
+            subcriterion_valid = True
+            evidence = subcriterion_data['evidence']['success']
         else:
-            logger.info('Issues found by markdownlint')
+            evidence = subcriterion_data['evidence']['failure']
+
+        if evidence:
+            logger.info(evidence)
+
+        doc_file_type = 'Markdown'
+        doc_file_standard = 'markdownlint'
+
+        subcriteria.append({
+            'id': subcriterion,
+            'description': subcriterion_data['description'] % doc_file_type,
+            'valid': subcriterion_valid,
+            'evidence': evidence % (doc_file_type, doc_file_standard),
+            'standard': self.standard
+        })
+
+        requirement_level = subcriterion_data['requirement_level']
+        self.valid = subcriterion_valid
+        if (
+            (not subcriterion_valid) and
+            (requirement_level in ['MUST'])
+        ):
+            self.valid = False
 
         data_to_return = {}
         for rule_issue in data:
@@ -44,5 +85,6 @@ class MarkdownLintValidator(sqaaas_utils.BaseValidator):
 
         return {
             'valid': self.valid,
+            'subcriteria': subcriteria,
             'data_unstructured': data_to_return
         }
