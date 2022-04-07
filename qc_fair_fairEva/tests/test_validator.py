c61665766164d89d92bdd73f7b9d008087020010
ferag <aguilarf@ifca.unican.es>
2022-04-07 14:16:54 +0200

    FAIR EVA plugin - new output

--- qc_fair_fairEva/tests/test_validator.py
+++ qc_fair_fairEva/tests/test_validator.py
@@ -1,11 +1,11 @@
 import pytest
 from types import SimpleNamespace
 
-from report2sqaaas_plugins_fairEvaluator.main import fairEvaluatorValidator
+from report2sqaaas_plugins_fairEva.main import fairEvaValidator
 
 
 @pytest.fixture
-def fairEvaluator_stdout():
+def fairEva_stdout():
     return """
     {
    "accessible":{
@@ -65,22 +65,22 @@ def fairEvaluator_stdout():
 
 
 @pytest.fixture
-def validator_opts(fairEvaluator_stdout):
+def validator_opts(fairEva_stdout):
     class_args = {
-        'validator': 'fairEvaluator',
-        'stdout': fairEvaluator_stdout
+        'validator': 'fairEva',
+        'stdout': fairEva_stdout
     }
     return SimpleNamespace(**class_args)
 
 
 @pytest.fixture
 def validator(validator_opts):
-    return fairEvaluatorValidator(validator_opts)
+    return fairEvaValidator(validator_opts)
 
 
 @pytest.mark.dependency()
 def test_is_validate_method_defined(validator_opts):
-    assert fairEvaluatorValidator(validator_opts).validate()
+    assert fairEvaValidator(validator_opts).validate()
 
 
 @pytest.mark.dependency(depends=["test_is_validate_method_defined"])
