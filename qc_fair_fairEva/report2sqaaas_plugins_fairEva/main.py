b12fc4efa782e8cf8ae6ce3eeac2970fc1abd36c
ferag <aguilarf@ifca.unican.es>
2022-04-11 10:15:03 +0200

    Style fixes

--- qc_fair_fairEva/report2sqaaas_plugins_fairEva/main.py
+++ qc_fair_fairEva/report2sqaaas_plugins_fairEva/main.py
@@ -29,10 +29,8 @@ class fairEva(sqaaas_utils.BaseValidator):
                     result.append({"id": json_res[sb][key]['name'],
                                    "valid": valid,
                                    "description": json_res[sb][key]['msg'],
-                                   "evidence": "Indicator: %s | Check: https://doi.org/10.15497/rda00050" 
-                                   % json_res[sb][key]['name'],
-                                    }
-                                  )
+                                   "evidence": "Indicator: %s | Check: https://doi.org/10.15497/rda00050"
+                                   % json_res[sb][key]['name']})
         if len(result) > 0:
             self.valid = True
 
