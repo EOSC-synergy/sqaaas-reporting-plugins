f86d7167ce39d9f444f160b96d8ff1e624cd041b
ferag <aguilarf@ifca.unican.es>
2022-04-11 10:28:18 +0200

    Style fixes

--- qc_fair_fairEva/setup.py
+++ qc_fair_fairEva/setup.py
@@ -11,8 +11,8 @@ setup(
     url='http://github.com/eosc-synergy/sqaaas-reporting-plugins',
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General 
-        Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: GNU General ' 
+        'Public License v3 or later (GPLv3+)',
         'Environment :: Plugins',
         'Development Status :: 3 - Alpha',
     ],
