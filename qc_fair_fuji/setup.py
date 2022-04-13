56ca5c95580ffdcce79c541c861f56a092e5db4c
wilkos-dans <wilko.steinhoff@dans.knaw.nl>
2022-04-13 14:46:47 +0200

    Report RDA-indicators

--- qc_fair_fuji/setup.py
+++ qc_fair_fuji/setup.py
@@ -1,18 +1,17 @@
 from setuptools import find_packages
 from setuptools import setup
 
-
 setup(
     name='report2sqaaas-plugin-fuji',
     version='1.0.0',
     description='Output validator for the F-UJI tool',
-    author='EOSC Synergy Task3.3',
+    author='Wilko Steinhoff',
     author_email='wilko.steinhoff@dans.knaw.nl',
-    url='http://github.com/eosc-synergy/sqaaas-reporting-plugins',
-	# For a list of valid classifiers, see https://pypi.org/classifiers/
+    url='https://github.com/eosc-synergy/sqaaas-reporting-plugins',
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        ('License :: OSI Approved :: GNU General Public License v3 or later '
+         '(GPLv3+)'),
         'Environment :: Plugins',
         'Development Status :: 3 - Alpha',
     ],
