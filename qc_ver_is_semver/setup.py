d5f5c862960a459521420dc00492c0c98b81786e
Pablo Orviz <orviz@ifca.unican.es>
2022-05-12 12:27:13 +0200

    Install dependencies from requirements.txt

--- qc_ver_is_semver/setup.py
+++ qc_ver_is_semver/setup.py
@@ -1,5 +1,35 @@
+import os
+
 from setuptools import find_packages
 from setuptools import setup
+from urllib.parse import urlparse
+
+
+def load_requirements():
+    """
+    Install dependencies from 'requirements.txt'.
+
+    If the file contains a pip-supported git+https' location for the package,
+    this method transforms it to setuptools' way. Some code has been taken from
+    https://stackoverflow.com/a/53069528
+    """
+    thelibFolder = os.path.dirname(os.path.realpath(__file__))
+    requirementPath = thelibFolder + '/requirements.txt'
+    install_requires = []
+    if os.path.isfile(requirementPath):
+        with open(requirementPath) as f:
+            install_requires = f.read().splitlines()
+    install_requires_filtered = []
+    for req in install_requires:
+        if not req.startswith('#'):
+            url_parsed = urlparse(req)
+            fragment = url_parsed.fragment
+            if fragment:
+                fragment = fragment.split('=')[-1]
+                req = '@'.join([fragment, req])
+            install_requires_filtered.append(req)
+
+    return install_requires_filtered
 
 
 setup(
@@ -17,6 +47,7 @@ setup(
         'Environment :: Plugins',
         'Development Status :: 3 - Alpha',
     ],
+    install_requires=load_requirements(),
     packages=find_packages(),
     entry_points={
         'sqaaas.validators': [
