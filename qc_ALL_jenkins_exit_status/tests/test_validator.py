e435d9377f1649a0486abf10ed1bc992d7c518d8
Pablo Orviz <orviz@ifca.unican.es>
2022-06-10 13:04:59 +0200

    Move to JenkinsExitStatus validator

--- qc_ALL_jenkins_exit_status/tests/test_validator.py
+++ qc_ALL_jenkins_exit_status/tests/test_validator.py
@@ -3,36 +3,27 @@ import pathlib
 import pytest
 from types import SimpleNamespace
 
-from report2sqaaas_plugins_no_output_parsing.main import \
-    NoOutputParsingValidator
+from report2sqaaas_plugins_jenkins_exit_status.main import \
+    JenkinsExitStatusValidator
 
 
 @pytest.fixture
-def no_output_parsing_stdin(request):
-    file = pathlib.Path(request.node.fspath.strpath)
-    stdout = file.with_name('stdin.json')
-    with stdout.open() as fp:
-        json_data = json.load(fp)
-        return json_data
-
-
-@pytest.fixture
-def validator_opts(no_output_parsing_stdin):
+def validator_opts():
     class_args = {
-        'validator': 'no_output_parsing',
-        'stdin': no_output_parsing_stdin
+        'validator': 'jenkins_exit_status',
+        'status': 'SUCCESS'
     }
     return SimpleNamespace(**class_args)
 
 
 @pytest.fixture
 def validator(validator_opts):
-    return NoOutputParsingValidator(validator_opts)
+    return JenkinsExitStatusValidator(validator_opts)
 
 
 @pytest.mark.dependency()
 def test_is_validate_method_defined(validator_opts):
-    assert NoOutputParsingValidator(validator_opts).validate()
+    assert JenkinsExitStatusValidator(validator_opts).validate()
 
 
 @pytest.mark.dependency(depends=["test_is_validate_method_defined"])
