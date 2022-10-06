9c8b044f00682f402df19aa3c360614b5aa641aa
Pablo Orviz <orviz@ifca.unican.es>
2022-10-06 14:21:37 +0200

    Fix: right way to check whether 'tool_name' is defined

--- qc_ALL_jenkins_exit_status/report2sqaaas_plugins_jenkins_exit_status/main.py
+++ qc_ALL_jenkins_exit_status/report2sqaaas_plugins_jenkins_exit_status/main.py
@@ -34,7 +34,7 @@ class JenkinsExitStatusValidator(sqaaas_utils.BaseValidator):
             evidence = subcriterion_data['evidence']['success']
         else:
             evidence = subcriterion_data['evidence']['failure']
-        if self.opts.tool_name:
+        if 'tool_name' in self.opts:
             evidence = evidence % self.opts.tool_name
 
         requirement_level = subcriterion_data['requirement_level']
