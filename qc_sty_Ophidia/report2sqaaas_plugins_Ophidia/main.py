cf3b957c1c8b388cceca278b17257ec99bb2deca
PalomoIFCA <palomo@ifca.unican.es>
2025-07-04 14:16:42 +0200

    added list of failed cases

--- qc_sty_Ophidia/report2sqaaas_plugins_Ophidia/main.py
+++ qc_sty_Ophidia/report2sqaaas_plugins_Ophidia/main.py
@@ -37,7 +37,14 @@ class OphidiaValidator(sqaaas_utils.BaseValidator):
             sub["hint"]=''
             subcriteria.append(sub)
             
-
+        for file_index in range(len(validation["failed_list"])):
+            sub={}
+            sub["id"]= validation["failied_list"][file_index]
+            sub["valid"]= False
+            sub["description"]="Is workflow valid?"
+            sub["evidence"]=validation["reasons_list"][file_index]
+            sub["hint"]=''
+            subcriteria.append(sub)
         return {
             "valid": res,
             "subcriteria": subcriteria,
